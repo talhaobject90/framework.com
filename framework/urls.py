0701911398b4b5bb38eb61d7896fdfc40d4d5e1b
talha <talha@object90.com>
2015-05-29 13:53:52 +0530

    New Templates

--- framework/urls.py
+++ framework/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -13,10 +14,30 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^dashboard/',views.dashboard, name='dashboard'),
     url(r'^dashboard/', 'django.contrib.auth.views.login'),
+    
+    # Register & Login URLs
     url(r'^logout/$', logout_page),
     url(r'^login/$', 'django.contrib.auth.views.login'), # If user is not login it will redirect to login page
     url(r'^register/$', register),
     url(r'^register/success/$', register_success),
+    
+    # Workflow urls
+    url(r'^project-info/',views.project_info),
+    url(r'^introduction/',views.introduction),
+    url(r'^background/',views.background),
+    url(r'^prod-info/',views.prod_info),
+    url(r'^features/',views.features),
+    url(r'^non-func-1/',views.non_func_1),
+    url(r'^non-func-2/',views.non_func_2),
+    url(r'^non-func-3/',views.non_func_3),
+    url(r'^environment/',views.environment),
+    url(r'^add-dev-cons/',views.add_dev_consideration),
+    url(r'^post-dev/',views.post_dev),
+    url(r'^use-case/',views.use_case),
+    url(r'^io-config/',views.io_config),
+    
+    
+    
     
     url(r'^admin/', include(admin.site.urls)),
 )+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- framework/views.py
+++ framework/views.py
@@ -11,5 +11,54 @@ def index(request):
 def dashboard(request):
     return render(request, 'dashboard/admin_dashboard.html')
 
+
+def project_info(request):
+    return render(request, 'workflow/project_info.html')
+
+def introduction(request):
+    return render(request, 'workflow/introduction.html')
+
+def background(request):
+    return render(request, 'workflow/background.html')
+
+def prod_info(request):
+    return render(request, 'workflow/prod_info.html')
+
+def features(request):
+    return render(request, 'workflow/features.html')
+
+def non_func_1(request):
+    return render(request, 'workflow/non_func_1.html')
+
+def non_func_2(request):
+    return render(request, 'workflow/non_func_2.html')
+
+def non_func_3(request):
+    return render(request, 'workflow/non_func_3.html')
+
+def environment(request):
+    return render(request, 'workflow/environment.html')
+
+def add_dev_consideration(request):
+    return render(request, 'workflow/add_dev_consideration.html')
+
+def post_dev(request):
+    return render(request, 'workflow/post_dev.html')
+
+def use_case(request):
+    return render(request, 'workflow/use_case.html')
+
+def use_case(request):
+    return render(request, 'workflow/use_case.html')
+
+def io_config  (request):
+    return render(request, 'workflow/io_config.html')
+
+
+
+
+
+
+
  
 
