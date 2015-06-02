a40803790601580b059fc7ca1b70b778e4c391ad
talha <talha@object90.com>
2015-06-02 22:26:28 +0530

    Json Try removed Save file okay

--- projectspecs/views.py
+++ projectspecs/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import View
 from django.shortcuts import render_to_response, get_object_or_404 ,render
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.http import HttpResponse , HttpResponseRedirect
 from django.core import serializers
 from django.template import RequestContext
 from django.conf import settings
@@ -36,19 +36,8 @@ def project_info(request):
             path_to_file = "{0}/db/"+request.session['file_name']+".json"
             path = path_to_file.format(settings.PROJECT_ROOT)
             file_data = open(path)
-            if(os.stat(path).st_size != 0):
-                try:
-                    json.load(file_data)
-                except ValueError:
-                    json_data = ''
-                else:
-                    with open(path) as f:
-                        json_data = json.load(f)
-            else:
-                #del request.session['file_name']
-                return HttpResponse("yes")
-            
-            
+            with open(path) as f:
+                json_data = json.load(f)
         else:  # if new project is opened
             #del request.session['file_name']
             path = "{0}/db/tmp.json".format(settings.PROJECT_ROOT)
@@ -63,16 +52,9 @@ def project_info(request):
 
 
 def new_project(request):
-        del request.session['file_name']
-        json_data =''
-        context_data = {'json_data':    json_data,
-                        'file_name':request.session.get('file_name', None)}
-        return render_to_response(
-        'workflow/project_info.html',
-        context_data,  
-        RequestContext(request)
-        )
-
+        if request.session.get('file_name', None) != None:
+            del request.session['file_name']
+        return HttpResponseRedirect("/projectspecs/project-info/")
     
 def introduction(request):
     return render(request, 'workflow/introduction.html')
